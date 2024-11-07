759c56b705162c28cbe68d1b3e837982efeb70bc
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-11-07 17:18:26 +0100

    cards created

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
-const postNumber = 6; 
+const displayCards = document.getElementById('cards-display')
 
-fetch('https://jsonplaceholder.typicode.com/photos?_limit=' + postNumber)
-.then((res) => res.json)
+fetch('https://jsonplaceholder.typicode.com/photos?_limit=6')
+.then((res) => res.json())
 .then (posts => {
-    console.log(posts.id);
-    
+
+    console.log(posts);
+
+    posts.forEach((post) => {
+
+        displayCards.innerHTML +=`
+        <div class="col-4">
+          <div class="card h-100">
+            <img src="${post.url}" alt="">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+          </div>
+         </div>`
+    });
 })
+console.log('test')
