eed187837a52ab5fe5a69d4a93324e020d8ff1d6
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-11-07 17:36:39 +0100

    final mods

--- js/script.js
+++ js/script.js
@@ -11,8 +11,8 @@ fetch('https://jsonplaceholder.typicode.com/photos?_limit=6')
         displayCards.innerHTML +=`
         <div class="col-4 my-3">
           <div class="card h-100 ">
-            <img src="${post.url}" alt="">
-            <p>${post.title}</p>
+            <img src="${post.url}"class="p-3" alt="">
+            <p class="m-3 fs-5">${post.title}</p>
           </div>
          </div>`
     });
