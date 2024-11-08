dd68ca5b41402d3a61a972a03094af33b7d0db46
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-11-08 18:23:06 +0100

    complete, data attribute add and used to change src of img of the layover

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const displayCards = document.getElementById("cards-display");
 const layoverdisplay = document.getElementById("layover");
-const closeButtonLO = document.getElementById("close-button");
+const closeButtonLo = document.getElementById("close-button");
+const layoverImg = document.querySelector("#layover-img img")
 
 fetch("https://jsonplaceholder.typicode.com/photos?_limit=6")
   .then((res) => res.json())
@@ -11,26 +12,34 @@ fetch("https://jsonplaceholder.typicode.com/photos?_limit=6")
       displayCards.innerHTML += `
         <div class="col-4 my-3">
           <div class="card h-100 ">
-            <img src="${post.url}"class="p-3" alt="">
+            <img src="${post.thumbnailUrl}"class="p-3" id="curretimg" alt="" data-bigurl="${post.url}">
             <p class="m-3 fs-5">${post.title}</p>
           </div>
          </div>`;
+        //  console.log(post.thumbnailUrl);
+        
     });
+
+    
+    
 
     const displayCardEl = document.querySelectorAll("#cards-display .card");
 
     displayCardEl.forEach((cardEl) => {
       cardEl.addEventListener("click", function () {
-        layoverdisplay.classList.remove('d-none')
-        console.log(cardEl);
-        
-       
+        const imgEl = cardEl.querySelector("img");
+        console.log(imgEl);
+        const bigUrl = imgEl.getAttribute("data-bigurl")
+        console.log(bigUrl);
 
+        layoverImg.src = bigUrl;       
+        
+        layoverdisplay.classList.remove('d-none')
       });
     });
 
 
-    closeButtonLO.addEventListener('click', () => {
+    closeButtonLo.addEventListener('click', () => {
       layoverdisplay.classList.add('d-none')
     })
 
